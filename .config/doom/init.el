6a580b4bc7f2b64248397a0f78f14cee86d615a2
Niko Komninos <nikokomninos@gmail.com>
2025-07-12 16:05:55 -0400

    .

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -159,7 +159,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       (python +lsp +tree-sitter)           ; beautiful is better than ugly
+       (python +lsp +tree-sitter +pyright)           ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
