e9cb533e59483e07f8f9ce1693a6a3e69c638ada
Niko Komninos <nikokomninos@gmail.com>
2025-08-02 16:45:30 -0400

    feat: update doom-emacs config

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -174,7 +174,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       (web +lsp +tailwind)              ; the tubes
+       (web +lsp)              ; the tubes
        ;;yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
