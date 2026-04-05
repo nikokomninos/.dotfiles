23621085e4d09c9da4b895d96fae790b7d405eda
Niko Komninos <nk@nikokomninos.com>
2026-04-05 13:35:51 -0400

    update nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -129,9 +129,18 @@ require("lazy").setup({
     {
       'windwp/nvim-autopairs',
       event = "InsertEnter",
-      config = true
-      -- use opts = {} for passing setup options
-      -- this is equivalent to setup({}) function
+      config = function()
+        local npairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
+
+        npairs.setup({})
+
+        npairs.add_rule(
+          Rule("$", "$", "typst"),
+          Rule("*", "*", "typst"),
+          Rule("_", "_", "typst")
+        )
+      end
     },
 
     {
